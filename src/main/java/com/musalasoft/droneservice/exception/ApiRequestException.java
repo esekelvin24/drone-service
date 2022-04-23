b95a891326d06e0d81d69a91a1d1a4c22a6295ba
Ese Kelvin Uvbiekpahor <eseboy24@gmail.com>
2022-04-23 14:45:33 +0100

    Adding a custom exception message which will be displayed to users

---
+++ src/main/java/com/musalasoft/droneservice/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.musalasoft.droneservice.exception;
+
+public class ApiRequestException extends RuntimeException{
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
