d3ecbb66f4482caa2f78376283d9c4d31aa6ef29
Ese Kelvin Uvbiekpahor <eseboy24@gmail.com>
2022-04-25 10:46:23 +0100

    adding a drone battery drainage and recharge battery log model

---
+++ src/main/java/com/musalasoft/droneservice/models/DroneBatteryAuditLog.java
@@ -0,0 +1,38 @@
+package com.musalasoft.droneservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DroneBatteryAuditLog {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long Id;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "drone_id", referencedColumnName = "id")
+    private Drone drone;
+
+    @Enumerated(EnumType.STRING)
+    private droneBatterProcessType processType;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "drone_state_id", referencedColumnName = "id")
+    private State state;
+
+    private int previousBatteryPercentage;
+    private int currentBatteryPercentage;
+    private Date timeStamp;
+
+
+}
